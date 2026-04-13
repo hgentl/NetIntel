b827b902dbf6014d0c4ce4320aab7ff1abff0cf1
Macbook <macbook@Macbooks-MacBook-Pro.local>
2026-04-13 11:48:47 +0100

    added concurrency features

--- cmd/check.go
+++ cmd/check.go
@@ -3,10 +3,11 @@ package cmd
 import (
 	"fmt"
 	"strings"
-	"time"
+	"sync"
 
 	"netintel/internal/analyser"
 	"netintel/internal/collector"
+	"netintel/internal/models"
 	"netintel/internal/scorer"
 
 	"github.com/spf13/cobra"
@@ -14,51 +15,74 @@ import (
 
 var checkCmd = &cobra.Command{
 	Use:   "check [url]",
-	Short: "CHeck a website",
-	Args:  cobra.ExactArgs(1),
+	Short: "Check a website",
+	Args:  cobra.MinimumNArgs(1),
 
 	Run: func(cmd *cobra.Command, args []string) {
-		url := args[0]
+		var wg sync.WaitGroup
 
-		if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
-			url = "https://" + url
+		resultsChan := make(chan *models.Result)
+		errorsChan := make(chan error)
+
+		for _, inputURL := range args {
+			wg.Add(1)
+
+			go func(url string) {
+				defer wg.Done()
+
+				if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+					url = "https://" + url
+				}
+
+				result, err := collector.CheckWebsite(url)
+				if err != nil {
+					errorsChan <- fmt.Errorf("failed for %s: %v", url, err)
+					return
+				}
+
+				resultsChan <- result
+			}(inputURL)
 		}
 
-		result, err := collector.CheckWebsite(url)
-		if err != nil {
-			fmt.Printf("Request failed for %s: %v\n", url, err)
-			return
+		// Close channels when done
+		go func() {
+			wg.Wait()
+			close(resultsChan)
+			close(errorsChan)
+		}()
+
+		// Handle results
+		for result := range resultsChan {
+			printResult(result)
 		}
 
-		fmt.Println("URL:", result.URL)
-		fmt.Println("Status:", result.HTTP.Status)
-		fmt.Println("Latency:", result.HTTP.Latency)
-
-		if result.HTTP.Server != "" {
-			fmt.Println("Server:", result.HTTP.Server)
+		// Handle errors
+		for err := range errorsChan {
+			fmt.Println("Error:", err)
 		}
-
-		fmt.Println("Final URL:", result.HTTP.FinalURL)
-		fmt.Println("Redirects:", result.HTTP.RedirectCount)
-
-		if !result.TLS.Expiry.IsZero() {
-			daysLeft := int(time.Until(result.TLS.Expiry).Hours() / 24)
-			fmt.Println("TLS Issuer:", result.TLS.Issuer)
-			fmt.Println("TLS Days Left:", daysLeft)
-		}
-
-		fmt.Println("IP Adresses:", result.DNS.IPs)
-		findings := analyser.Analyse(result)
-		score, risk := scorer.Calculate(findings)
-
-		fmt.Println("\nFindings:")
-
-		for _, f := range findings {
-			fmt.Printf("[%s] (%s) %s\n", f.Severity, f.Type, f.Message)
-		}
-
-		fmt.Printf("\nRisk Score: %d (%s)\n", score, risk)
 	},
+}
+
+func printResult(result *models.Result) {
+	fmt.Println("=================================")
+	fmt.Println("URL:", result.URL)
+	fmt.Println("Status:", result.HTTP.Status)
+	fmt.Println("Latency:", result.HTTP.Latency)
+
+	if result.HTTP.Server != "" {
+		fmt.Println("Server:", result.HTTP.Server)
+	}
+
+	findings := analyser.Analyse(result)
+	score, risk := scorer.Calculate(findings)
+
+	fmt.Println("\nFindings:")
+	for _, f := range findings {
+		fmt.Printf("[%s] (%s) %s\n", f.Severity, f.Type, f.Message)
+	}
+
+	fmt.Printf("\nRisk Score: %d (%s)\n", score, risk)
+	fmt.Println("=================================")
 }
 
 func init() {
